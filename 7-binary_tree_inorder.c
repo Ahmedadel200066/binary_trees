119191141f20c77356cda6df3ed7e37b1d471e26
Ahmedadel200066 <medo.adel200@gmail.com>
2023-10-31 20:03:23 +0200

    edit

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a binary tree using in-order 
+ * binary_tree_inorder - goes through a binary tree using in-order
  * @tree: is a pointer to the root node 3mr 7a7a remix
  * @func: is a pointer to a function to call for every node
-**/
+ **/
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
